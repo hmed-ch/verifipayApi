600fb48fc8bc5f9db351714cf7ae24a7699ac4be
hmed-ch <60175549+hmed-ch@users.noreply.github.com>
2020-04-19 12:50:16 +0100

    Update __init__.py

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -1,1 +1,11 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.adress import Adress
+from swagger_server.models.customer import Customer
+from swagger_server.models.merchant_urls import MerchantUrls
+from swagger_server.models.order import Order
+from swagger_server.models.order_line import OrderLine
 
